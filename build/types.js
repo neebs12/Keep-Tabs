1ba2bddd665e284aab1a0f6f3e5d6d2437674fc3
Jason Aricheta <51255216+neebs12@users.noreply.github.com>
2022-08-13 15:44:25 +1200

    Delete build directory

--- build/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENVS = void 0;
-// ENV types
-var ENVS;
-(function (ENVS) {
-    ENVS["dev"] = "dev";
-    //  <-- add as more envs come online
-})(ENVS = exports.ENVS || (exports.ENVS = {}));
