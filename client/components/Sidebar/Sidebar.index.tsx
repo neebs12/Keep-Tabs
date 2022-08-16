8124bd1123f0433d153b4beb3e03d6758d01ff37
Jason Aricheta <jason.aricheta@gmail.com>
2022-08-16 17:26:56 +1200

    aesthetic of the side bar completed

--- client/components/Sidebar/Sidebar.index.tsx
+++ client/components/Sidebar/Sidebar.index.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
-import { Divider, Drawer, Toolbar, Typography } from '@mui/material'
-
+import { Avatar, Button, Container, Divider, Drawer, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material'
+import NoteAddIcon from '@mui/icons-material/NoteAdd'
+import BallotIcon from '@mui/icons-material/Ballot'
+import DoneAllIcon from '@mui/icons-material/DoneAll'
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom'
+import ViewListIcon from '@mui/icons-material/ViewList'
+import WorkIcon from '@mui/icons-material/Work';
+import SkateboardingIcon from '@mui/icons-material/Skateboarding';
+import PeopleIcon from '@mui/icons-material/People';
+import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
 
 const drawerWidth = 240
 
 const Sidebar = () => {
+
+  // const catergories = ['Work', 'Personal', 'Social']
+  const categoriesAvatarPair = {
+    Work: <WorkIcon />,
+    Personal: <SelfImprovementIcon />,
+    Social: <PeopleIcon />,
+    Fun: <SkateboardingIcon />
+  }
+
   return (
     <Drawer
       variant="permanent"
@@ -14,17 +31,75 @@ const Sidebar = () => {
         // flexShrink: 0,
         width: drawerWidth,
         flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' }
+        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        // display: 'block'
       }}
     >
       <Toolbar />
-      <Typography component="div" variant="h6">
-        First Item List
-      </Typography>
+      <Button 
+        // variant="contained" 
+        disableElevation={true} 
+        startIcon={<NoteAddIcon />} 
+        size="large" 
+        sx={{mt: 1, mb: 0.5, ml: 0.5, mr: 0.5}}
+      > Add New Note </Button>
+
+      <Button 
+        // variant="contained" 
+        disableElevation={true} 
+        startIcon={<BallotIcon />} 
+        size="large" 
+        sx={{m: 0.5}}
+      >Add Categories</Button>      
+
+      <Container sx={{display: 'flex', justifyContent:"center"}}><Typography variant="caption" color="primary">Completion</Typography></Container>
       <Divider />
-      <Typography component="div" variant="h6">
-        Second Item List
-      </Typography>      
+
+      <Button 
+        // variant="contained" 
+        disableElevation={true} 
+        startIcon={<ViewListIcon />} 
+        size="large" 
+        sx={{m: 0.5}}
+      >All</Button>      
+
+      <Button 
+        // variant="contained" 
+        disableElevation={true} 
+        startIcon={<DoneAllIcon />} 
+        size="large" 
+        sx={{m: 0.5}}
+      >Completed</Button>       
+
+      <Button 
+        // variant="contained" 
+        disableElevation={true} 
+        startIcon={<HourglassBottomIcon />} 
+        size="large" 
+        sx={{m: 0.5}}
+      >Incomplete</Button>       
+
+      <Container sx={{display: 'flex', justifyContent:"center"}}><Typography variant="caption" color="primary">Categories</Typography></Container>
+      <Divider />
+
+      <List color="primary">
+        {Object.entries(categoriesAvatarPair).map((pair) => {
+          const [name, JSXAvatar] = pair
+          return( // cat is unique
+            <ListItem key={name} disablePadding>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar>
+                    {JSXAvatar}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={name} />
+              </ListItemButton>
+            </ListItem>
+          )
+        })}
+      </List>
+
     </Drawer>
   )
 }
